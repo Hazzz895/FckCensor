4ec7beb8680184b058d0da44ff7e3b954c2a63cf
Hazzz895 <kornilovily4@gmail.com>
2026-03-30 15:04:11 +0400

    добавление локальных треков из контекстного меню

--- script.js
+++ script.js
@@ -49,43 +49,66 @@
         return result;
     };
 
-    function getReplaced(trackId) {
-        if (!trackId) return null;
-        url = null;
-        src = null;
-        if (localTracks[trackId]) {
-            url = localTracks[trackId];
-            src = "local";
-        }
-        else if (remoteTracks[trackId]) {
-            url = remoteTracks[trackId];
-            src = "remote";
-        }
-        return url ? { url, src } : null;
+    // хранение подменных треков
+    // из базы данных
+    let localTracks = {};
+
+    // открытие базы данных
+    function openDB() {
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open("FckCensorData", 1);
+
+            request.onupgradeneeded = (event) => {
+                const db = event.target.result;
+                if (!db.objectStoreNames.contains("tracks")) {
+                    db.createObjectStore("tracks", { keyPath: "id" });
+                }
+            };
+
+            request.onsuccess = () => resolve(request.result);
+            request.onerror = () => reject(request.error);
+        });
     }
 
-    function updateLocalTracks() {
+    // первоначальная загрузка треков из базы данных
+    openDB().then(db => {
+        const tx = db.transaction("tracks", 'readonly');
+        const store = tx.objectStore("tracks");
+        const request = store.getAll();
+
+        request.onsuccess = () => {
+            request.result.forEach(({ id, data }) => {
+                localTracks[id] = URL.createObjectURL(data);
+            });
+            console.debug("Tracks from local database:", localTracks);
+        };
+    });
+
+    // из папки assets
+    let assetsTracks = {};
+    function updateAssetsTracks() {
         fetch("http://localhost:2007/assets?name=FckCensor")
             .then(response => response.json())
             .then(data => {
                 Object.keys(data.files).forEach(file => {
                     id = file.split(".")[0]
                     url = "http://localhost:2007/assets/" + file + "?name=FckCensor&"
-                    localTracks[id] = url;
+                    assetsTracks[id] = url;
                 });
-                console.debug(localTracks);
+                console.debug("Tracks from assets:", assetsTracks);
             });
     }
 
-    let localTracks = {};
-    updateLocalTracks();
+    updateAssetsTracks();
+
+    // из репозитория
     let remoteTracks = {};
 
     fetch("https://raw.githubusercontent.com/Hazzz895/FckCensorData/refs/heads/main/list.json")
         .then(response => response.json())
         .then(data => {
             remoteTracks = data.tracks;
-            console.debug(remoteTracks)
+            console.debug("Tracks from remote repository:", remoteTracks);
         });
 
     // основной код аддона, выполняется после инициализации DI
@@ -113,15 +136,73 @@
         };
     }
 
+    // получение ссылки на трек
+    function getReplaced(trackId) {
+        if (!trackId) return null;
+        url = null;
+        src = null;
+        if  (localTracks[trackId]) {
+            url = localTracks[trackId];
+            src = "local";
+        }
+        else if (assetsTracks[trackId]) {
+            url = assetsTracks[trackId];
+            src = "assets";
+        }
+        else if (remoteTracks[trackId]) {
+            url = remoteTracks[trackId];
+            src = "remote";
+        }
+        return url ? { url, src } : null;
+    }
+
+    // контекстное меню подмены (сохранение в indexeddb)
     function onContextMenuClick(entity, item) {
+        entity = window.pulsesyncApi?.getCurrentTrack() ?? entity;
         const trackId = entity.id;
         const replaced = getReplaced(trackId);
+        // если трек НЕ подменен, то открывается пикер файлов и затем он сохраняется в бд
         if (!replaced) {
-            
+            window.showOpenFilePicker({
+                types:
+                [
+                    {
+                        description: 'Аудио-файлы',
+                        accept: { 'audio/*': ['.mp3', '.wav', '.ogg', '.flac'] }
+                    }
+                ],
+                multiple: false 
+            })
+            .then(async (fileHandles) => {
+                console.log("Selected file handles:", fileHandles);
+                const fileHandle = fileHandles[0];
+
+                const file = await fileHandle.getFile();
+                if (!file.type.startsWith("audio/")) {
+                    alert("Выбранный файл не является аудио-файлом.");
+                    return;
+                }
+                console.log("Selected file:", file);
+                const db = await openDB();
+
+                localTracks[trackId] = URL.createObjectURL(file);
+
+                const tx = db.transaction("tracks", 'readwrite');
+                const store = tx.objectStore("tracks");
+                
+                store.add({ id: trackId, data: file });
+                updateReplaceItem(entity, item);
+                console.debug("Added track " + trackId + " to local tracks");
+            });
         }
-        else {
+        // если трек есть в базе данных, то удаление
+        else if (replaced.src == "local") {
             delete localTracks[trackId];
-            localStorage.setItem("fckcensor_localtracks", JSON.stringify(localTracks));
+            openDB().then(db => {
+                const tx = db.transaction("tracks", 'readwrite');
+                const store = tx.objectStore("tracks");
+                store.delete(trackId);
+            });
             updateReplaceItem(entity, item);
             console.debug("Removed track " + trackId + " from local tracks");
         }
@@ -131,31 +212,38 @@
         const span = item.querySelector('span')
         const replaced = !!getReplaced(entity?.id);
 
-        span.childNodes[0].firstElementChild.setAttribute("xlink:href", "/icons/sprite.svg#" + (replaced ? "close" : "pensil") + "_xxs");
+        span.childNodes[0].firstElementChild.setAttribute("xlink:href", "/icons/sprite.svg#" + (replaced ? "close" : "edit") + "_xxs");
         span.childNodes[1].nodeValue = replaced ? "Удалить замену" : "Подменить трек";
     }
 
-    /*const observer = new MutationObserver(mutations => {
+    // следим за dom-изменениями
+    const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
             mutation.addedNodes.forEach(node => {
                 if (!(node instanceof HTMLElement)) return;
+                // появилось ли контекстное меню трека?
                 const trackMenu = node?.querySelector("[data-test-id='TRACK_CONTEXT_MENU']:not(:has([data-test-id='CONTEXT_MENU_REPLACE_BUTTON']))");
                 if (trackMenu) {
                     const button = trackMenu.ariaLabelledByElements[0];
+                    // а относится ли контекстное меню к плееру?
                     if (button.matches("[data-test-id='PLAYERBAR_DESKTOP_CONTEXT_MENU_BUTTON'], [data-test-id='FULLSCREEN_PLAYER_CONTEXT_MENU_BUTTON']")) {
                         const entity = window.pulsesyncApi?.getCurrentTrack();
                         const replaced = getReplaced(entity?.id);
-                        if (!entity || !replaced || replaced?.src == "remote") return;
+                        if (!entity || replaced?.src != "local") return;
+
                         const downloadItem = trackMenu.querySelector('[data-test-id="CONTEXT_MENU_DOWNLOAD_BUTTON"]')
                         const replaceItem = downloadItem.cloneNode(true)
+
                         replaceItem.setAttribute('data-test-id', 'CONTEXT_MENU_REPLACE_BUTTON');
+
                         updateReplaceItem(entity, replaceItem);
                         replaceItem.addEventListener('click', () => onContextMenuClick(entity, replaceItem));
+
                         downloadItem.parentElement.insertBefore(replaceItem, downloadItem.nextSibling);
                     }
                 }
             })
         });
     });
-    observer.observe(document.body, { childList: true, subtree: true });*/
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
